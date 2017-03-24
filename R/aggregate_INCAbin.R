0c4be2faba57383b48e43d5f1d96504004b98f91
Christoph Schürz <christoph.schuerz@boku.ac.at>
2017-03-24 18:31:24 +0100

    implemented progress bar in aggregate_INCA

--- R/aggregate_INCAbin.R
+++ R/aggregate_INCAbin.R
@@ -1,4 +1,31 @@
 
+#' Title
+#'
+#' @param bin_pth
+#' @param basin_shp
+#' @param bin_crs
+#' @param bin_ext
+#' @param shp_index
+#'
+#' @importFrom pasta %_% %//%
+#' @importFrom dplyr mutate mutate_at select matches starts_with left_join
+#'   vars funs group_by summarize_all
+#' @importFrom tibble as_tibble add_column
+#' @import     lubridate
+#' @import foreach
+#' @import doSNOW
+#' @importFrom parallel makeCluster stopCluster detectCores
+#' @importFrom magrittr %>% set_colnames subtract
+#' @importFrom ncdf4 nc_open nc_close ncvar_get ncatt_get
+#' @importFrom raster raster rasterToPoints extent crs intersect
+#'   removeTmpFiles
+#' @importFrom sp SpatialPoints SpatialPointsDataFrame spTransform
+#'   SpatialPolygonsDataFrame
+#'
+#' @return
+#' @export
+#'
+#' @examples
 aggregate_INCAbin <- function(bin_pth, basin_shp, bin_crs, bin_ext, shp_index) {
 
 # Fetch header and binary files names from binary folder --------------
@@ -51,17 +78,24 @@ aggregate_INCAbin <- function(bin_pth, basin_shp, bin_crs, bin_ext, shp_index) {
 
   t_step <- seq(24*3600/header$NBLOCKS, 24*3600, length.out = header$NBLOCKS)
 
-  tm <- system.time({
-  sub_aggr <- list()
+  cl <- makeCluster(detectCores())
+  registerDoSNOW(cl)
 
-  for( i_bil in bil_lst){
-    t_0 <- strsplit(i_bil, "\\_|\\.") %>%
+  print(paste("Aggregating", length(bil_lst), "binary files:"))
+  prgr_bar <- txtProgressBar(min = 0, max = length(bil_lst), initial = 0, style = 3)
+  progress <- function(n) setTxtProgressBar(prgr_bar, n)
+  opts <- list(progress = progress)
+
+  sub_aggr <- foreach( i_bil = 1:length(bil_lst),
+                       .packages = c("dplyr", "tibble", "lubridate", "magrittr", "pasta"),
+                       .options.snow = opts) %dopar% {
+    t_0 <- strsplit(bil_lst[i_bil], "\\_|\\.") %>%
       unlist() %>%
       .[2] %>%
       paste("00:00") %>%
       ymd_hm()
 
-    bil_i <- readBin(con = bin_pth%//%i_bil,
+    bil_i <- readBin(con = bin_pth%//%bil_lst[i_bil],
                      what = "numeric",
                      n = header$NROWS * header$NCOLS * header$NBLOCKS,
                      size = 4) %>%
@@ -71,7 +105,7 @@ aggregate_INCAbin <- function(bin_pth, basin_shp, bin_crs, bin_ext, shp_index) {
       mutate(idx = 1:nrow(.))
 
     # Aggregate variable for subbasins
-    sub_aggr[[i_bil]] <- left_join(idx_area, bil_i, by = "idx") %>%
+    aggr_i <- left_join(idx_area, bil_i, by = "idx") %>%
       mutate_at(vars(starts_with("time")), funs(.*fraction)) %>%
       select(-idx) %>%
       group_by(basin) %>%
@@ -87,47 +121,48 @@ aggregate_INCAbin <- function(bin_pth, basin_shp, bin_crs, bin_ext, shp_index) {
                  hour = hour(t_0 + t_step),
                  min  = minute(t_0 + t_step),
                  .before = 1)
+
+    return(aggr_i)
   }
+  stopCluster(cl)
+
   sub_aggr <- bind_rows(sub_aggr)
-
-
-  })
-
-  tm
 
 }
 
-# Testing -------------------------------------------------------------
-library(pasta)
-library(magrittr)
-library(dplyr)
-library(raster)
-library(tibble)
-library(rgdal)
-
-bin_pth <- "F:/mirror_H/ETP_AT/ETP_AT_Exe/input/T2M"
-bin_ext <- c(99000, 700000, 250000, 601000)
-bin_crs <- crs(basin_shp)
-shp_index <- "Subbasin"
-
-
-mt <- array(bil_i, dim = c(header$NCOLS, header$NROWS, header$NBLOCKS))
-
-mt <- bil_i[1:(header$NCOLS*header$NROWS)]
-
-mt[,,1] %>% View
-  raster() %>%
-  plot
-
-  idx_rst <-  matrix(data = 1:(header$NROWS * header$NCOLS),
-                     nrow = header$NCOLS)
-  idx_rst[1:5, 1:5] <- 200000
-
-  idx_rst%<>%
-    t() %>%
-    raster() %>%
-      assign_ext(., bin_ext) %>%
-      assign_crs(., crs(bin_crs))
+# # Testing -------------------------------------------------------------
+# library(pasta)
+# library(magrittr)
+# library(dplyr)
+# library(raster)
+# library(tibble)
+ # library(rgdal)
+# library(doSNOW)
+# library(parallel)
+#
+# bin_pth <- "F:/mirror_H/ETP_AT/ETP_AT_Exe/input/T2M"
+# bin_ext <- c(99000, 700000, 250000, 601000)
+# bin_crs <- crs(basin_shp)
+# shp_index <- "Subbasin"
+#
+#
+# mt <- array(bil_i, dim = c(header$NCOLS, header$NROWS, header$NBLOCKS))
+#
+# mt <- bil_i[1:(header$NCOLS*header$NROWS)]
+#
+# mt[,,1] %>% View
+#   raster() %>%
+#   plot
+#
+#   idx_rst <-  matrix(data = 1:(header$NROWS * header$NCOLS),
+#                      nrow = header$NCOLS)
+#   idx_rst[1:5, 1:5] <- 200000
+#
+#   idx_rst%<>%
+#     t() %>%
+#     raster() %>%
+#       assign_ext(., bin_ext) %>%
+#       assign_crs(., crs(bin_crs))
   #     projectRaster(crs = crs(basin_shp))
 
 
@@ -146,11 +181,11 @@ mt[,,1] %>% View
 
 
 # Load basin boundary shape file --------------------------------------
-bnd_dir <- "D:/UnLoadC3/00_RB_SWAT/raab_sb4/Watershed/Shapes/"
-bnd_file_name = "subs1.shp"
-basin_shp <- readOGR(paste(bnd_dir,bnd_file_name, sep = ""),
-                   layer = "subs1")
-
-
-plot(idx_poly)
-plot(basin_shp, add = T)
+# bnd_dir <- "D:/UnLoadC3/00_RB_SWAT/raab_sb4/Watershed/Shapes/"
+# bnd_file_name = "subs1.shp"
+# basin_shp <- readOGR(paste(bnd_dir,bnd_file_name, sep = ""),
+#                    layer = "subs1")
+#
+#
+# plot(idx_poly)
+# plot(basin_shp, add = T)
