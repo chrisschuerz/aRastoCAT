d107fcd387c007bfe0a4b247db2ead29449e70ce
Christoph Schürz <christoph.schuerz@boku.ac.at>
2017-03-28 07:59:56 +0200

    minor

--- R/testing/extracting for Raab.R
+++ R/testing/extracting for Raab.R
@@ -10,7 +10,7 @@ bin_pth <- "F:/mirror_H/ETP_AT/ETP_AT_Exe/input"
 obs_4 <- list()
 t_T <- system.time({
    obs_4$tmp <- aggregate_INCAbin(bin_pth = bin_pth%//%"T2M",
-                                  basin_shp = basin_4, bin_crs = crs(basin_shp),
+                                  basin_shp = basin_4, bin_crs = crs(basin_4),
                                   bin_ext = c(99500, 700500, 249500, 600500),
                                   shp_index = "Subbasin")
 
@@ -19,7 +19,7 @@ t_T[3]/60
 
 t_P <- system.time({
   obs_4$pcp <- aggregate_INCAbin(bin_pth = bin_pth%//%"RR",
-                                 basin_shp = basin_4, bin_crs = crs(basin_shp),
+                                 basin_shp = basin_4, bin_crs = crs(basin_4),
                                  bin_ext = c(99500, 700500, 249500, 600500),
                                  shp_index = "Subbasin")
 
@@ -37,7 +37,7 @@ basin_30 <- readOGR(paste(bnd_dir,bnd_file_name, sep = ""),
 obs_30 <- list()
 t_T <- system.time({
   obs_30$tmp <- aggregate_INCAbin(bin_pth = bin_pth%//%"T2M",
-                                 basin_shp = basin_30, bin_crs = crs(basin_shp),
+                                 basin_shp = basin_30, bin_crs = crs(basin_30),
                                  bin_ext = c(99500, 700500, 249500, 600500),
                                  shp_index = "Subbasin")
 
@@ -46,7 +46,7 @@ t_T[3]/60
 
 t_P <- system.time({
   obs_30$pcp <- aggregate_INCAbin(bin_pth = bin_pth%//%"RR",
-                                 basin_shp = basin_30, bin_crs = crs(basin_shp),
+                                 basin_shp = basin_30, bin_crs = crs(basin_30),
                                  bin_ext = c(99500, 700500, 249500, 600500),
                                  shp_index = "Subbasin")
 
