65536680abdc81f14452113a72e0aa7b1547d1ec
Christoph Schürz <christoph.schuerz@boku.ac.at>
2017-03-29 16:15:00 +0200

    implemented drop_col in aggregate_time()

--- R/aggregate_time.R
+++ R/aggregate_time.R
@@ -4,17 +4,31 @@
 #' @param time_int character time interval to aggregate, either 'year',
 #'   'mon', 'day', 'hour', or 'min'
 #' @param aggr_fun Aggregation function to apply
+#' @param drop_col logical to set whether unused time columns are dropped
+#'   (TRUE) or kept in the table (FALSE). If FALSE unused time columns
+#'   values are set to zero.
 #'
 #' @return Returns an aggregated time series tibble
 #' @export
 
-aggregate_time <- function(ts_tbl, time_int = "day", aggr_fun = mean) {
+aggregate_time <- function(ts_tbl, time_int = "day", aggr_fun = mean,
+                           drop_col = FALSE) {
   time_step <- c("year", "mon", "day", "hour", "min")
-  time_step <- time_step[1:which(time_step == time_int)] %>%
+  time_keep <- time_step[1:which(time_step == time_int)]
+  time_drop <- time_step[!(time_step %in% time_keep)]
+  time_grp  <- time_keep %>%
     lapply(., as.symbol)
-  #
-  ts_tbl %>%
-    group_by_(.dots = time_step) %>%
-    summarise_at(vars(-year, -mon, -day, -hour, - min), funs(aggr_fun)) %>%
+
+  ts_tbl %<>%
+    group_by_(.dots = time_grp) %>%
+    summarise_all(funs(aggr_fun)) %>%
     ungroup()
+
+  if(drop_col){
+    ts_tbl %>%
+      select(-one_of(time_drop))
+  } else {
+    ts_tbl %>%
+      mutate_at(vars(one_of(time_drop)), funs(.*0))
+  }
 }
